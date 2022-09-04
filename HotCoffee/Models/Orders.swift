03248002cc03c334e66c7b47f1d8f2f80121939c
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-09-04 00:36:29 -0300

    Models do Orders

---
+++ HotCoffee/Models/Orders.swift
@@ -0,0 +1,29 @@
+//
+//  Orders.swift
+//  HotCoffee
+//
+//  Created by Henrique Alves Batochi on 04/09/22.
+//
+
+import Foundation
+
+enum CoffeeType: String, Codable {
+    case cappuccino
+    case latte
+    case espressino
+    case cortado
+}
+
+enum CoffeeSize: String, Codable {
+    case small
+    case medium
+    case large
+}
+
+struct Order: Codable {
+    
+    let name: String
+    let email: String
+    let type: CoffeeType
+    let size: CoffeeSize
+}
